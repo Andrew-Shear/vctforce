734626117bb2295b806de04651666e6660404c1a
Andrew-Shear <ashear@vt.edu>
2026-07-28 23:21:45 -0400

    i'm ready for the big one :)

--- scrape.py
+++ scrape.py
@@ -5,49 +5,59 @@ import json
 from bs4 import BeautifulSoup
 import random
 
-for year in range(2023, 2027):
-    matchIDs = []
-    with open(f"matchIDs/{year}.txt", "r") as file:
-        matchIDs = file.read().splitlines()
+matchIDs = []
+with open(f"VCL_data/VCL_matchIDs.txt", "r") as file:
+    matchIDs = file.read().splitlines()
 
-    with open(f"newData_{year}.py", "w") as file:
-        file.write("data = [\n")
-        for i in range(len(matchIDs)):
-            match_id = matchIDs[i]
-            response = requests.get(f"https://www.vlr.gg/" + match_id + "?tab=economy")
-            if response.status_code == 429:
-                print("TOO FAST")
-                exit(1)
-            elif response.status_code != 200:
-                print("ERROR")
-                print(response.text)
-                exit(1)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            for tableCover in soup.find_all('table', class_='wf-table-inset mod-econ')[1::2]:
-                table = tableCover.find_all('td')
-                teamNames = [name.text.strip() for name in table[0].find_all('div', class_='team')]
-                roundEcos = []
-                roundWins = []
-                roundSpent = []
-                for td in table[1:]:
-                    Round = td.find_all('div', class_='rnd-sq')
-                    if len(Round) == 0: continue
-                    if "mod-win" in Round[0]["class"]:
-                        roundWins.append([teamNames[0],])
-                    else:
-                        roundWins.append([teamNames[1],])
-                    if "mod-ct" in Round[0]["class"] or "mod-ct" in Round[1]["class"]: # the defense won this round
-                        roundWins[-1].append("ct")
-                    else:
-                        roundWins[-1].append("t")
+with open(f"VCL_data/data_VCL.py", "w") as file:
+    wait = 0
+    file.write("data = [\n")
+    for i in range(len(matchIDs)):
+        match_id = matchIDs[i]
+        response = requests.get(f"https://www.vlr.gg/" + match_id + "?tab=economy")
+        if response.status_code == 429:
+            print("TOO FAST")
+            exit(1)
+        elif response.status_code != 200:
+            print("ERROR")
+            print(response.text)
+            exit(1)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        mapNames = []
+        for mapBox in soup.find_all('div', class_='vm-stats-gamesnav-item')[1:]:
+            mapNames.append(mapBox.div.text.strip()[1:].strip()) # lol
 
-                    roundEcos.append([bank.text.strip()[:-1] for bank in td.find_all('div', class_='bank')])
-                    roundSpent.append([r.text.count("$") for r in Round])
-                        
-                file.write(str([teamNames, roundWins, roundEcos, roundSpent]))
-                file.write(",\n")
+        mapNameIndex = 0
+        for tableCover in soup.find_all('table', class_='wf-table-inset mod-econ')[1::2]:
+            table = tableCover.find_all('td')
+            teamNames = [name.text.strip() for name in table[0].find_all('div', class_='team')]
+            roundEcos = []
+            roundWins = []
+            roundSpent = []
+            for td in table[1:]:
+                Round = td.find_all('div', class_='rnd-sq')
+                if len(Round) == 0: continue
+                if "mod-win" in Round[0]["class"]:
+                    roundWins.append([teamNames[0],])
+                else:
+                    roundWins.append([teamNames[1],])
+                if "mod-ct" in Round[0]["class"] or "mod-ct" in Round[1]["class"]: # the defense won this round
+                    roundWins[-1].append("ct")
+                else:
+                    roundWins[-1].append("t")
 
-            time.sleep(2 + 2*random.random())
-            print(f"{year}: {len(matchIDs)-i-1} matches left")
-        file.write("]\n")
+                roundEcos.append([float(bank.text.strip()[:-1]) for bank in td.find_all('div', class_='bank')])
+                roundSpent.append([r.text.count("$") for r in Round])
+                    
+            file.write(str({"link": response.url, "teams": teamNames, "map": mapNames[mapNameIndex], "roundWins": roundWins, "roundEcos": roundEcos, "roundSpent": roundSpent}))
+            file.write(",\n")
+            mapNameIndex += 1
+
+        wait += 1
+        if wait > 8:
+            time.sleep(1 + 1.5*random.random())
+            wait = 0
+        print(f"{len(matchIDs)-i-1} matches left")
+
+    file.write("]\n")
 
