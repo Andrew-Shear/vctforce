d016631b715fd90bad4907668df01c5a3f396475
Andrew-Shear <ashear@vt.edu>
2026-07-30 12:53:48 -0400

    got updated VCT data

--- scrape.py
+++ scrape.py
@@ -14,12 +14,12 @@ with open(f"VCT_data/data_VCT.py", "w") as file:
     wait = 0
     file.write("data = [\n")
     for i in range(len(matchIDs)):
-        match_id = matchIDs[i]
+        matchID = matchIDs[i]
         foundMatch = False
         attempts = 0
         while not foundMatch:
             try:
-                response = requests.get(f"https://www.vlr.gg/" + match_id + "?tab=economy")
+                response = requests.get(f"https://www.vlr.gg/" + matchID)
                 foundMatch = True
             except:
                 attempts += 1
@@ -38,42 +38,106 @@ with open(f"VCT_data/data_VCT.py", "w") as file:
             print(response.text)
             exit(1)
         soup = BeautifulSoup(response.text, 'html.parser')
-        mapNames = []
-        for mapBox in soup.find_all('div', class_='vm-stats-gamesnav-item')[1:]:
-            mapNames.append(mapBox.div.text.strip()[1:].strip()) # lol
 
-        mapNameIndex = 0
-        for tableCover in soup.find_all('table', class_='wf-table-inset mod-econ')[1::2]:
-            table = tableCover.find_all('td')
+        foundMatch = False
+        attempts = 0
+        while not foundMatch:
+            try:
+                response = requests.get(f"https://www.vlr.gg/" + matchID + "?tab=economy")
+                foundMatch = True
+            except:
+                attempts += 1
+                if attempts >= 5:
+                    print("giving up")
+                    exit(1)
+                print("response error occured, waiting 5 seconds before trying again.")
+                time.sleep(5)
+
+
+        if response.status_code == 429:
+            print("TOO FAST")
+            exit(1)
+        elif response.status_code != 200:
+            print("ERROR")
+            print(response.text)
+            exit(1)
+        soupEconomy = BeautifulSoup(response.text, 'html.parser')
+
+        matchData = {}
+
+        for gameCover in soupEconomy.find('div', class_='vm-stats-container').find_all('div', class_='vm-stats-game'):
+            gameID = gameCover["data-game-id"]
+            if gameID == 'all':
+                continue
+
+            table = gameCover.find_all('table', class_='wf-table-inset')
+            if len(table) == 0:
+                # this map is not available (always means it wasn't played afaik)
+                continue
+
+            table = table[1].find_all('td')
             teamNames = [name.text.strip() for name in table[0].find_all('div', class_='team')]
+
             roundEcos = []
-            roundWins = []
             roundSpent = []
             for td in table[1:]:
+                Round = td.find_all('div', class_='rnd-sq')
+                if len(Round) == 0:
+                    # not a real round
+                    continue
+
+                roundEcos.append([float(bank.text.strip()[:-1]) for bank in td.find_all('div', class_='bank')])
+                roundSpent.append([r.text.count("$") for r in Round])
+
+            matchData[gameID] = {"roundEcos": roundEcos, "roundSpent": roundSpent}
+
+
+        for gameCover in soup.find('div', class_='vm-stats-container').find_all('div', class_='vm-stats-game'):
+            gameID = gameCover["data-game-id"]
+            if gameID == 'all' or gameID not in matchData: # if it's the overall data or there was no economy data saved
+                continue
+            gameHeader = gameCover.find('div', class_='vm-stats-game-header')
+            mapName = gameHeader.find('div', class_='map').div.span.text.strip().split("\t")[0] # :)
+
+            vlrRounds = gameCover.find('div', class_='vlr-rounds').find_all('div', class_='vlr-rounds-row-col')
+
+            roundWins = []
+            roundWinMethods = []
+            teams = [name.text.strip() for name in vlrRounds[0].find_all('div', class_='team')]
+
+            for td in vlrRounds[1:]:
+                if (not td.has_attr('title')) or td['title'] == "":
+                    # it's not a real round
+                    continue
                 Round = td.find_all('div', class_='rnd-sq')
                 if len(Round) == 0: continue
                 if "mod-win" in Round[0]["class"]:
                     roundWins.append([teamNames[0],])
+                    winnerDiv = Round[0]
                 else:
                     roundWins.append([teamNames[1],])
-                if "mod-ct" in Round[0]["class"] or "mod-ct" in Round[1]["class"]: # the defense won this round
+                    winnerDiv = Round[1]
+
+                if "mod-ct" in winnerDiv["class"]:
                     roundWins[-1].append("ct")
                 else:
                     roundWins[-1].append("t")
+                roundWinMethods.append(winnerDiv.find("img")["src"][20:-5])
 
-                roundEcos.append([float(bank.text.strip()[:-1]) for bank in td.find_all('div', class_='bank')])
-                roundSpent.append([r.text.count("$") for r in Round])
                     
-            if mapNameIndex >= len(mapNames):
-                # it failed to pull the map names b.c. it was a bo1 - just don't bother tbh
-                file.write(str({"link": response.url, "teams": teamNames, "map": "n/a", "roundWins": roundWins, "roundEcos": roundEcos, "roundSpent": roundSpent}))
-            else:
-                file.write(str({"link": response.url, "teams": teamNames, "map": mapNames[mapNameIndex], "roundWins": roundWins, "roundEcos": roundEcos, "roundSpent": roundSpent}))
+            file.write(str({"matchID": matchID,
+                            "gameID": gameID,
+                            "teams": teamNames,
+                            "map": mapName,
+                            "roundWins": roundWins,
+                            "roundWinMethods": roundWinMethods,
+                            "roundEcos": matchData[gameID]["roundEcos"],
+                            "roundSpent": matchData[gameID]["roundSpent"]
+                           }))
             file.write(",\n")
-            mapNameIndex += 1
 
         wait += 1
-        if wait > 8:
+        if wait > 6:
             time.sleep(1 + 1.5*random.random())
             wait = 0
         print(f"{len(matchIDs)-i-1} matches left")
