deb14a65d2ca003a927a6c6f36674f63d3de6402
Andrew-Shear <ashear@vt.edu>
2026-08-23 00:18:57 -0400

    added a better bar graph to show the data

--- analyze_scoreline.py
+++ analyze_scoreline.py
@@ -33,9 +33,9 @@ for game in data:
 
     scorelines = forced_scorelines if secondPistolLoserForced else not_forced_scorelines
 
-    if sPLoserWins == 12:
+    if sPLoserWins == 8 and scorelines == forced_scorelines:
         print(roundEcos[13][secondPistolLoserIndex])
-        #print("forced" if scorelines == forced_scorelines else "not_forced")
+        print("won" if secondPistolWinner != gameWinner else "lost")
 
     if (sPLoserWins, sPWinnerWins) not in scorelines:
         scorelines[(sPLoserWins, sPWinnerWins)] = [0, 1]
@@ -84,30 +84,34 @@ for game in data:
 
 
 # FORCING
-fig, (forced_ax, not_forced_ax) = plt.subplots(1, 2, figsize=(18, 8))
-forced_ax.set_title("VCT 2023-Present Data: Chance of Winning a Map After You Lose 2nd Pistol and Force")
-not_forced_ax.set_title("VCT 2023-Present Data: Chance of Winning a Map After You Lose 2nd Pistol and Don't Force")
+fig, ax = plt.subplots(figsize=(18, 8))
+ax.set_title("VCT 2023-Present Data: Chance of Winning a Map After You Lose 2nd Pistol and Force/Don't Force")
+forced_sorted_scorelines = sorted([[scoreline, percent[0]/percent[1]*100] for scoreline, percent in forced_scorelines.items()], key=lambda x: x[0][0])
+not_forced_sorted_scorelines = sorted([[scoreline, percent[0]/percent[1]*100] for scoreline, percent in not_forced_scorelines.items()], key=lambda x: x[0][0])
 
-for scorelines, ax in ((forced_scorelines, forced_ax), (not_forced_scorelines, not_forced_ax)):
-    sorted_scorelines = sorted([[scoreline, percent[0]/percent[1]] for scoreline, percent in scorelines.items()], key=lambda x: x[1] + 0.0001*x[0][0])
+res = ax.grouped_bar({'Forced': [s[1] for s in forced_sorted_scorelines],
+                      'Not Forced': [s[1] for s in not_forced_sorted_scorelines]},
+                     tick_labels=[s[0] for s in forced_sorted_scorelines], group_spacing=0.5, colors=("mediumblue", "forestgreen"))
+for container in res.bar_containers:
+    ax.bar_label(container, padding=3, fmt="%.1f%%")
 
-    for scoreline in sorted_scorelines:
-        print(f"{scoreline[0]}: {scoreline[1]*100:.2f}%")
+print("forced")
+for scoreline in forced_sorted_scorelines:
+    print(f"{scoreline[0]}: {scoreline[1]:.2f}%")
+print("not_forced")
+for scoreline in not_forced_sorted_scorelines:
+    print(f"{scoreline[0]}: {scoreline[1]:.2f}%")
 
-    x = [str(s[0]) for s in sorted_scorelines]
-    y = [s[1]*100 for s in sorted_scorelines]
-    colors = plt.colormaps['RdYlGn'](mcolors.Normalize(vmin=0, vmax=100)(y))
+#colors = plt.colormaps['RdYlGn'](mcolors.Normalize(vmin=0, vmax=100)(y))
 
-    ax.bar(x, y, color=colors)
+ax.set_xlabel("Scoreline")
+ax.set_ylabel("% Chance of Winning the Map")
 
-    ax.set_xlabel("Scoreline")
-    ax.set_ylabel("% Chance of Winning the Map")
+ax.set_yticks(list(range(0, 101, 10)))
+ax.tick_params("x", rotation=45, rotation_mode="xtick")
+ax.legend()
 
-    ax.set_yticks(list(range(0, 101, 10)))
-    ax.grid(axis="y")
-    ax.tick_params("x", rotation=45, rotation_mode="xtick")
-    plt.tight_layout()
-
+plt.tight_layout()
 plt.show()
 
 #sorted_scorelines = sorted([[scoreline, percent[0]/percent[1]] for scoreline, percent in scorelines.items()], key=lambda x: x[1] + 0.0001*x[0][0])
