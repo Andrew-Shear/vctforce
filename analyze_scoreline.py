727e7d88849e23b277b43bb8e4548f6d892ffb4d
Andrew-Shear <ashear@vt.edu>
2026-08-22 23:55:40 -0400

    messing around:

--- analyze_scoreline.py
+++ analyze_scoreline.py
@@ -6,65 +6,130 @@ import numpy as np
 with open("VCT_data/data_VCT.json") as file:
     data = orjson.loads(file.read())
 
-scorelines = {}
+# NORMAL
+#scorelines = {}
+
+# FORCING
+# manual adjustment rahhhh
+not_forced_scorelines = {(1, 12): [0, 1]}
+forced_scorelines = {(12, 1): [1, 1]}
+
 for game in data:
     roundWins = [r[0] for r in game["roundWins"]]
     if len(roundWins) == 0: continue
+
+    # FORCING
+    if len(roundWins) <= 13: continue
     gameWinner = roundWins[-1]
-    winnerWins = roundWins[:12].count(gameWinner)
-    loserWins = 12 - winnerWins
-    if winnerWins != loserWins:
-        if (loserWins, winnerWins) not in scorelines:
-            scorelines[(loserWins, winnerWins)] = [0, 0]
-        if (winnerWins, loserWins) not in scorelines:
-            scorelines[(winnerWins, loserWins)] = [0, 0]
-        scorelines[(loserWins, winnerWins)][1] += 1
-        scorelines[(winnerWins, loserWins)][1] += 1
-        scorelines[(winnerWins, loserWins)][0] += 1
+    secondPistolWinner = roundWins[12]
+    sPWinnerWins = roundWins[:12].count(secondPistolWinner)
+    sPLoserWins = 12 - sPWinnerWins
+    sPWinnerWins += 1
+    
+    roundEcos = game["roundEcos"]
+    teams = game["teams"]
+    secondPistolLoserIndex = 1 if teams[0] == secondPistolWinner else 0
+    secondPistolLoserForced = roundEcos[13][secondPistolLoserIndex] < 4.5
 
-    for i in range(12, min(len(roundWins)-1, 24)):
-        if roundWins[i] == gameWinner:
-            winnerWins += 1
-        else:
-            loserWins += 1
-        if winnerWins == loserWins:
-            continue
-        elif winnerWins >= loserWins:
-            scoreline = (loserWins, winnerWins)
-        else:
-            scoreline = (winnerWins, loserWins)
+    scorelines = forced_scorelines if secondPistolLoserForced else not_forced_scorelines
 
-        if max(winnerWins, loserWins) >= 13:
-            break
-        if (loserWins, winnerWins) not in scorelines:
-            scorelines[(loserWins, winnerWins)] = [0, 0]
-        if (winnerWins, loserWins) not in scorelines:
-            scorelines[(winnerWins, loserWins)] = [0, 0]
-        scorelines[(loserWins, winnerWins)][1] += 1
-        scorelines[(winnerWins, loserWins)][1] += 1
-        scorelines[(winnerWins, loserWins)][0] += 1
+    if sPLoserWins == 12:
+        print(roundEcos[13][secondPistolLoserIndex])
+        #print("forced" if scorelines == forced_scorelines else "not_forced")
+
+    if (sPLoserWins, sPWinnerWins) not in scorelines:
+        scorelines[(sPLoserWins, sPWinnerWins)] = [0, 1]
+    else:
+        scorelines[(sPLoserWins, sPWinnerWins)][1] += 1
+
+    if secondPistolWinner != gameWinner:
+        scorelines[(sPLoserWins, sPWinnerWins)][0] += 1
 
 
-sorted_scorelines = sorted([[scoreline, percent[0]/percent[1]] for scoreline, percent in scorelines.items()], key=lambda x: x[1] + 0.0001*x[0][0])
+    # NORMAL
+    #gameWinner = roundWins[-1]
+    #winnerWins = roundWins[:12].count(gameWinner)
+    #loserWins = 12 - winnerWins
 
-#for scoreline in sorted_scorelines:
-#    print(f"{scoreline[0]}: {scoreline[1]*100:.2f}%")
+    #if winnerWins != loserWins:
+    #    if (loserWins, winnerWins) not in scorelines:
+    #        scorelines[(loserWins, winnerWins)] = [0, 0]
+    #    if (winnerWins, loserWins) not in scorelines:
+    #        scorelines[(winnerWins, loserWins)] = [0, 0]
+    #    scorelines[(loserWins, winnerWins)][1] += 1
+    #    scorelines[(winnerWins, loserWins)][1] += 1
+    #    scorelines[(winnerWins, loserWins)][0] += 1
 
-fig, ax = plt.subplots(figsize=(18, 8))
+    #for i in range(12, min(len(roundWins)-1, 24)):
+    #    if roundWins[i] == gameWinner:
+    #        winnerWins += 1
+    #    else:
+    #        loserWins += 1
+    #    if winnerWins == loserWins:
+    #        continue
+    #    elif winnerWins >= loserWins:
+    #        scoreline = (loserWins, winnerWins)
+    #    else:
+    #        scoreline = (winnerWins, loserWins)
 
-x = [str(s[0]) for s in sorted_scorelines]
-y = [s[1]*100 for s in sorted_scorelines]
-colors = plt.colormaps['RdYlGn'](mcolors.Normalize(vmin=0, vmax=100)(y))
+    #    if max(winnerWins, loserWins) >= 13:
+    #        break
+    #    if (loserWins, winnerWins) not in scorelines:
+    #        scorelines[(loserWins, winnerWins)] = [0, 0]
+    #    if (winnerWins, loserWins) not in scorelines:
+    #        scorelines[(winnerWins, loserWins)] = [0, 0]
+    #    scorelines[(loserWins, winnerWins)][1] += 1
+    #    scorelines[(winnerWins, loserWins)][1] += 1
+    #    scorelines[(winnerWins, loserWins)][0] += 1
 
-ax.bar(x, y, color=colors)
 
-ax.set_xlabel("Scoreline")
-ax.set_ylabel("% Chance of Winning the Map")
-ax.set_title("VCT 2023-Present Data: Chance of Winning a Map From Certain Scorelines")
+# FORCING
+fig, (forced_ax, not_forced_ax) = plt.subplots(1, 2, figsize=(18, 8))
+forced_ax.set_title("VCT 2023-Present Data: Chance of Winning a Map After You Lose 2nd Pistol and Force")
+not_forced_ax.set_title("VCT 2023-Present Data: Chance of Winning a Map After You Lose 2nd Pistol and Don't Force")
 
-ax.set_yticks(list(range(0, 101, 10)))
-ax.grid(axis="y")
-ax.tick_params("x", rotation=45, rotation_mode="xtick")
-plt.tight_layout()
+for scorelines, ax in ((forced_scorelines, forced_ax), (not_forced_scorelines, not_forced_ax)):
+    sorted_scorelines = sorted([[scoreline, percent[0]/percent[1]] for scoreline, percent in scorelines.items()], key=lambda x: x[1] + 0.0001*x[0][0])
+
+    for scoreline in sorted_scorelines:
+        print(f"{scoreline[0]}: {scoreline[1]*100:.2f}%")
+
+    x = [str(s[0]) for s in sorted_scorelines]
+    y = [s[1]*100 for s in sorted_scorelines]
+    colors = plt.colormaps['RdYlGn'](mcolors.Normalize(vmin=0, vmax=100)(y))
+
+    ax.bar(x, y, color=colors)
+
+    ax.set_xlabel("Scoreline")
+    ax.set_ylabel("% Chance of Winning the Map")
+
+    ax.set_yticks(list(range(0, 101, 10)))
+    ax.grid(axis="y")
+    ax.tick_params("x", rotation=45, rotation_mode="xtick")
+    plt.tight_layout()
+
 plt.show()
 
+#sorted_scorelines = sorted([[scoreline, percent[0]/percent[1]] for scoreline, percent in scorelines.items()], key=lambda x: x[1] + 0.0001*x[0][0])
+#
+#for scoreline in sorted_scorelines:
+#    print(f"{scoreline[0]}: {scoreline[1]*100:.2f}%")
+#
+#fig, ax = plt.subplots(figsize=(18, 8))
+#
+#x = [str(s[0]) for s in sorted_scorelines]
+#y = [s[1]*100 for s in sorted_scorelines]
+#colors = plt.colormaps['RdYlGn'](mcolors.Normalize(vmin=0, vmax=100)(y))
+#
+#ax.bar(x, y, color=colors)
+#
+#ax.set_xlabel("Scoreline")
+#ax.set_ylabel("% Chance of Winning the Map")
+#ax.set_title("VCT 2023-Present Data: Chance of Winning a Map After You Lose 2nd Pistol and Don't Force and Lose Again")
+#
+#ax.set_yticks(list(range(0, 101, 10)))
+#ax.grid(axis="y")
+#ax.tick_params("x", rotation=45, rotation_mode="xtick")
+#plt.tight_layout()
+#plt.show()
+#
